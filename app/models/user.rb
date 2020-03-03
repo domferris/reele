f20d067f29f899562d918d793b9afde14e5958f6
Elena Petrova <07387909591elena@gmail.com>
2020-03-03 15:23:46 +0000

    add username, bio, specialty, to the user model, added fields to new and edit registration, add validation to username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  # has_one_attached :photo
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
 end
