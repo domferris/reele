9fc15a3bb5a011c12794130c96a3489f29753e2e
Jack Holdsworth <jackch90@gmail.com>
2020-03-03 12:47:48 +0000

    Edited routes and created a project view file ready for test

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/profile', to: 'users#show'
 
-  resources :projects do
-    resources :comments, only: :create
+  resources :projects, only: [:index, :show, :destroy] do
+    resources :comments, only: [:index, :new, :create]
   end
 
-  resources :comments, only: :destroy
+  resources :users, only: [] do
+    resources :projects, only: [:new, :create, :edit, :update]
+  end
+
+  resources :comments, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
