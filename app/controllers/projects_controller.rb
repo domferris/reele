7ad7d606f80e1fdb2d92f604a4b936e8bbfc79b9
juancarlosasensio <jasensio@ufm.edu>
2020-03-03 15:14:11 +0000

    Create Pages#profile view, fixed routes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,7 +20,7 @@ class ProjectsController < ApplicationController
     @project = Project.new(project_params)
     @project.user = current_user
     if @project.save
-      redirect_to project_path(@project)
+      redirect_to user_projects_path(@project)
     else
       render :new
     end
