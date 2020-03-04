b0585c554109f2f3fa3ee394f04397e912d7516c
Dominic Ferris <ferris.dominic@gmail.com>
2020-03-04 15:50:22 +0000

    new proj formatting, added media_type collection constant and audio_file to project via activestorage upload

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,9 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_one_attached :photo
+  has_one_attached :audio_file
+
+  MEDIA_TYPE = ['audio', 'video']
 
   validates :title, :media_type, presence: true
 end
