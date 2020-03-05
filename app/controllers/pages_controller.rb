5ca0bb3ba098d97ffba5d24e7a153d25d14ccea8
Jack Holdsworth <jackch90@gmail.com>
2020-03-05 11:38:38 +0000

    Routing for categories works via params, with dummy category and associations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,15 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if params[:query].present?
+      @project_list = Project.where(categories: params[:query])
+    else
+      @project_list = Project.all
+    end
   end
 
   def dashboard
     @user = current_user
   end
 end
+
