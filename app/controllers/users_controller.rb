c4585a770171f71d516ac7a06c3cd4f41d773c3d
Jack Holdsworth <jackch90@gmail.com>
2020-03-06 13:56:20 +0000

    Show/hide logic for public and private projects on user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by_username(params[:username])
+    @featured = @user.projects.select { |p| p.published == true }
   end
 end
