e5e4dda7dc91878e23ecdf2cff64e8fcb37ccda7
Jack Holdsworth <jackch90@gmail.com>
2020-03-06 11:59:27 +0000

    Seeds created. Awaiting images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_151203) do
+ActiveRecord::Schema.define(version: 2020_03_06_113149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2020_03_05_151203) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "video_url"
+    t.string "category"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,21 @@
+require 'faker'
+
 puts "Destroying all users and projects"
 User.destroy_all
+Category.destroy_all
 
+
+######### Category creation ###########
 puts "Generating categories..."
 
-  cats = ["audio", "mastering", "mixing", "mad skills", "potato"]
+  cats = ["Mixing","Mastering","Audio Engineer", "Producer", "Sound Design", "Composition"]
   cats.each do |category|
     Category.create!(
       name: category
       )
   end
+
+######### Tyler creation ###########
 
 puts "Creating Tyler Kitty "
 
@@ -21,23 +28,70 @@ puts "Creating Tyler Kitty "
     email: "tyler@kitty.com",
     password: '******'
     )
-# file = URI.open(Rails.root.join('app','assets','images','iu-3.jpeg'))
-# user.photos.attach(io: file, filename: "#{user.name}.jpeg", content_type: 'image/jpeg')
 
-puts "Created User for Tyler Kitty"
+
+tyler_project_mixtape = Project.create!(
+    title: "Mixtape",
+    description: "A great mixtape where I play with some base tracks and some groovy melodies",
+    media_type: "video",
+    published: false,
+    user_id: User.first.id,
+    category: "Mixing",
+    video_url: "https://player.vimeo.com/video/304107088?color=ffffff&title=0&byline=0&portrait=0",
+    # file = URI.open(Rails.root.join('app','assets','images','iu-3.jpeg')),
+    # project.photo.attach(io: file, filename: "#{project.name}.jpeg", content_type: 'image/jpeg')
+    )
+
+# 5 * tyler projects
+
+puts "Created User and Projects for Tyler Kitty"
+
+
+
+######### User creation ###########
+
+# 10 * user
+
+puts "Creating 10 fake users..."
+
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    username: Faker::Internet.unique.username,
+    password: '******',
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    bio: Faker::Lorem.paragraph(sentence_count: 2),
+    specialty: 'a'
+    )
+end
+
+######### Project creation ###########
 
 puts "Creating new projects..."
 
-tyler_project_mixtape = Project.create!(
-  title: "Mixtape",
-  description: "A great mixtape where I play with some base tracks and some groovy melodies",
-  media_type: "video",
-  published: false,
-  user_id: User.first.id,
-  video_url: "https://player.vimeo.com/video/304107088?color=ffffff&title=0&byline=0&portrait=0",
+# 30 * projects
+
+20.times do
+  Project.create!(
+    title: Faker::Music.album,
+    description: Faker::Movies::PrincessBride.quote,
+    media_type: ["audio","video"].sample,
+    category: cats.sample,
+    published: true,
+    user_id: rand(2..11),
+    video_url: "https://player.vimeo.com/video/304107088?color=ffffff&title=0&byline=0&portrait=0",
+    # file = URI.open(Rails.root.join('app','assets','images','iu-3.jpeg'))
+    # project.photo.attach(io: file, filename: "#{project.name}.jpeg", content_type: 'image/jpeg')
   )
+end
+
 
 puts "All seeds for projects have been created successfully!"
+
+
+# file = URI.open(Rails.root.join('app','assets','images','iu-3.jpeg'))
+# user.photos.attach(io: file, filename: "#{user.name}.jpeg", content_type: 'image/jpeg')
 
 
 
