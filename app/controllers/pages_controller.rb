8b0ed35026096eb774f6d6e945095b8a9abd7170
Jack Holdsworth <jackch90@gmail.com>
2020-03-06 15:01:28 +0000

    Only published projects display across homepage, project show page, profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,9 +6,9 @@ class PagesController < ApplicationController
     if params[:query].present?
       # redirect_to :home if Category.find_by(name: params[:query]).projects.nil?
       # @project_list = Category.find_by(name: params[:query]).projects
-      @project_list = Project.where(category: params[:query])
+      @project_list = Project.where(published: true, category: params[:query])
     else
-      @project_list = Project.all
+      @project_list = Project.where(published: true)
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by_username(params[:username])
-    @featured = @user.projects.select { |p| p.published == true }
+    @published_projects = @user.projects.select { |p| p.published == true }
+
+    # @projects = @user.projec
   end
 end
