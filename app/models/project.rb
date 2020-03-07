10346ff6b3de706ab542802d24d6d0da8c205850
Jack Holdsworth <jackch90@gmail.com>
2020-03-07 12:11:03 +0000

    Added a bandcamp scraping method to Project model. Added option to embed audio into New Project form. Added audio_url column to Project to account for the embed.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,15 +1,32 @@
+require 'Nokogiri'
+require 'open-uri'
+
 class Project < ApplicationRecord
   belongs_to :user
   # has_many :project_categories
   # has_one :category, through: :project_categories
   has_many :comments, dependent: :destroy
-
   # CLOUDINARY RELATION -- REMOVED UNTIL STABLE
   # has_one_attached :photo
-
   has_one_attached :audio_file
-
   MEDIA_TYPE = ['audio', 'video']
-
   validates :title, :media_type, presence: true
+
+  def bandcamp_scrape(url)
+    html = open(url).read
+    doc = Nokogiri::HTML(html)
+    string = doc.css('meta[property="twitter:player"]').to_s
+    embed = string.scan(/https.*list=true/)
+    # split string into two portions for insertion into iframe
+    embed_split = embed[0].split('size=large')
+  end
+
+  def bandcamp_embed
+
+  end
 end
+
+# interpolating the return int othe recipe instance
+
+
+# <input type="text" class="embed_text" data-bind="value: code" readonly="">
