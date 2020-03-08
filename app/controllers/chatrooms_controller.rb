a5a1d6886a07ca6b1dbdea99114f140dbce8a31f
Dominic Ferris <ferris.dominic@gmail.com>
2020-03-08 13:12:24 +0000

    user now has chatrooms index, individual clickable chatrooms

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,8 @@
 class ChatroomsController < ApplicationController
+  def index
+    @chatrooms = Chatroom.where(sender_id: current_user).or(Chatroom.where(receiver_id: current_user)).order(:updated_at).reverse
+  end
+
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
