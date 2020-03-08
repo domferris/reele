17ed4debf3f4cf4794a1cb4def62ab095b7bc1c3
Jack Holdsworth <jackch90@gmail.com>
2020-03-08 13:27:19 +0000

    Added rough user availability inidcator to profile. Should revisit this with icons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_username(params[:username])
     @published_projects = @user.projects.select { |p| p.published == true }
-
-    # @projects = @user.projec
   end
 end
