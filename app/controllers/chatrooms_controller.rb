d7eb53aa9207bc6b89a689c781e0f67c1231816a
Dominic Ferris <ferris.dominic@gmail.com>
2020-03-09 12:49:14 +0000

    create new message now works on proj show

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -14,4 +14,20 @@ class ChatroomsController < ApplicationController
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
   end
+
+  def create
+    unless we_already_have_a_chatroom?
+      @chatroom = Chatroom.new
+      @chatroom.sender = current_user
+      @chatroom.receiver = User.find(params[:chatroom][:receiver_id])
+      @chatroom.save!
+    end
+    redirect_to chatrooms_path
+  end
+
+  private
+
+  def we_already_have_a_chatroom?
+    Chatroom.where(sender: current_user, receiver: User.find(params[:chatroom][:receiver_id])).present?
+  end
 end
