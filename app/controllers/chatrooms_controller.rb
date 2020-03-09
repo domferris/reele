82bbdeaf82c507de15295c87c188695208475c63
Dominic Ferris <ferris.dominic@gmail.com>
2020-03-09 00:38:50 +0000

    chats and message thread styling overall, chatrooms react to click -- adds active class

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,13 @@
 class ChatroomsController < ApplicationController
   def index
-    @chatrooms = Chatroom.where(sender_id: current_user).or(Chatroom.where(receiver_id: current_user)).order(:updated_at).reverse
+    # if params[:query].present?
+    #   @chatrooms = Chatroom.where("sender_id ILIKE ?", "%#{User.find_by_username(params[:query]).id}%")
+    # else
+      @chatrooms = Chatroom.where(sender_id: current_user).or(Chatroom.where(receiver_id: current_user)).order(:updated_at).reverse
+    # end
+
+    @chatroom = Chatroom.where(sender_id: current_user).or(Chatroom.where(receiver_id: current_user)).last
+    @message = Message.new
   end
 
   def show
