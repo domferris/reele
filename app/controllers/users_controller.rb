07445955bde2e4148dcc95bf8897201e8b41fa0d
juancarlosasensio <jasensio@ufm.edu>
2020-03-10 10:15:43 +0000

    Skip authentication for Users#show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :show ]
+
   def show
     @user = User.find_by_username(params[:username])
     @published_projects = @user.projects.select { |p| p.published == true }
-
-    # @projects = @user.projec
   end
 end
